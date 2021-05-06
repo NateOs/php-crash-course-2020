46023943417f2276e9eac9b568d973ecfe2fba91
nateos <sodjanathan@gmail.com>
2021-05-06 23:08:46 +0000

    includes

--- 10_include_require/01_website_skeleton/about.php
+++ 10_include_require/01_website_skeleton/about.php
@@ -1,17 +1,9 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<header>
-    <a href="index.php">Home</a>
-    <a href="about.php">About</a>
-</header>
+<?php
+//require can be used instead of include, however it breaks the code if there's an error
+//include_once only includes the referenced code once
+//include, includes the referenced code every time it is called
+include "partials/header.php"?>
+
 <h1>About us</h1>
 <footer>
     Copyright &copy; TheCodeholic</footer>
