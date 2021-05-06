05a2b1d3aa6ad2c7b45a480dd67ce605024f1f02
nateos <sodjanathan@gmail.com>
2021-05-06 23:34:36 +0000

    File System

--- 10_include_require/01_website_skeleton/index.php
+++ 10_include_require/01_website_skeleton/index.php
@@ -1,4 +1,9 @@
-<?php include "partials/header.php"?>
+<?php include "partials/header.php";
+  include_once "functions/ math.php";
+
+echo add(4, 5).'<br>';
+echo subtract(5, 4).'<br>';
+?>
 <h3>Georgia, Tbilisi 5&#8451;</h3>
 <h1>Welcome to my cool website</h1>
 <footer>
--- 11_fs/index.php
+++ 11_fs/index.php
@@ -1,16 +1,22 @@
 <?php
 // Magic constants
-
+echo __DIR__.'<br>'; //prints current dir where this file is located
+echo __FILE__.'<br>'; //prints exact file directory where this file is located
+echo __LINE__.'<br>'; //prints exact line of code, being this current line in the IDE
 // Create directory
-
+mkdir('test'); //creates a DIR 'test'
 // Rename directory
-
+rename('test', 'test2');
 // Delete directory
-
+rmdir('test2');
 // Read files and folders inside directory
-
+$files = scandir('./');
+echo '<pre>';
+var_dump($files);
+echo '<pre>';
 // file_get_contents, file_put_contents
-
+echo file_get_contents('lorem.txt');
+echo file_put_contents('sample.txt', 'Some contnet');
 // file_get_contents from URL
 
 // https://www.php.net/manual/en/book.filesystem.php
