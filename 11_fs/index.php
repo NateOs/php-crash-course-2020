0308aa05316aebf9648b56752fd69f81df3836a7
nateos <sodjanathan@gmail.com>
2021-05-09 14:23:12 +0000

    Files in PHP

--- 11_fs/index.php
+++ 11_fs/index.php
@@ -14,15 +14,31 @@ $files = scandir('./');
 echo '<pre>';
 var_dump($files);
 echo '<pre>';
+
 // file_get_contents, file_put_contents
 echo file_get_contents('lorem.txt');
-echo file_put_contents('sample.txt', 'Some contnet');
+echo file_put_contents('sample.txt', 'Some content');
+
 // file_get_contents from URL
+// $usersJSON = file_get_contents('https://jsonplaceholder.typicode.com/users');
+// echo '<pre>';
+// var_dump($usersJSON);
+// echo '<pre>';
+
+// $users = json_decode($usersJSON, true);
+// echo '<pre>';
+// var_dump($users);
+// echo '<pre>';
 
 // https://www.php.net/manual/en/book.filesystem.php
 // file_exists
+echo file_exists('sample.txt'); //true
 // is_dir
+
 // filemtime
+
 // filesize
+
 // disk_free_space
+
 // file
