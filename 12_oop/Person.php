bf3587729db333dfd25399b85c6d659ab26ea2b3
nateos <sodjanathan@gmail.com>
2021-05-09 20:03:34 +0000

    OOP Basics-php Class, Object, Instance

--- 12_oop/Person.php
+++ 12_oop/Person.php
@@ -1,15 +1,16 @@
 <?php
 
 class Person {
-    public $name;
-    public $surname;
-    private $age;
-    public static $counter = 0;
+    public string $name;
+    public string $surname;
+    private ?int $age;
+    public static int $counter = 0;
 
     public function __construct($name, $surname) {
         $this->name = $name;
         $this->surname = $surname;
         self::$counter++; // $this references the obj, self references the class, since static means the property is for the class  
+        $this->age = null; // ?int allows accepting null values as int
     }
 
     public function setAge($age){
--- 12_oop/index.php
+++ 12_oop/index.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once "Person.php";
+require_once "Student.php";
+
 // What is class and instance
     // Classes are programming language structures that define what class objects include in terms of data stored in variables also known
     //  as properties, and behavior of the objects defined by functions also known as methods.
@@ -13,21 +15,27 @@ require_once "Person.php";
 
 // Create instance of Person
 
-$p = new Person('Brad', 'Traversy');
-$p->setAge(30); /* trying to set the Age directly won't work because its a private property on the class, therefore you use 
-                *the method "setAge" on the function. */
-echo $p->getAge(30);
+// $p = new Person('Brad', 'Traversy');
+// $p->setAge(30); /* trying to set the Age directly won't work because its a private property on the class, therefore you use 
+//                 *the method "setAge" on the function. */
+// echo $p->getAge(30);
 
-echo '<pre>';
-var_dump($p);
-echo '</pre>';
+// echo '<pre>';
+// var_dump($p);
+// echo '</pre>';
 
-$p2 = new Person('John','Smith');
-$p3 = new Person('Jane','Samonas');
+// $p2 = new Person('John','Smith');
+// $p3 = new Person('Jane','Samonas');
 
-echo '<pre>';
-var_dump($p2);
-echo '</pre>';
-echo Person::getCounter(); //accessing the counter on the Person class
+// echo '<pre>';
+// var_dump($p2);
+// echo '</pre>';
+// echo Person::getCounter(); //accessing the counter on the Person class
 
 // Using setter and getter
+
+$student = new Student('Nathan', 'Sodja', '1234'); //because student extends person, it also requires the same args
+
+echo '<pre>';
+var_dump($student);
+echo '</pre>';
