1f97537e56143b2ed2db49456d66f5899d12dc95
nateos <sodjanathan@gmail.com>
2021-06-09 22:49:28 +0000

    rendering data from the server to html

--- 14_product_crud/index.php
+++ 14_product_crud/index.php
@@ -10,9 +10,9 @@ $statement = $pdo->prepare('SELECT * FROM PRODUCTS ORDER BY CREATE_DATE DESC');
 $statement->execute();
 $products = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-echo '<pre>';
-var_dump($products);
-echo '</pre>';
+// echo '<pre>';
+// var_dump($products);
+// echo '</pre>';
 
 ?>
 
@@ -31,45 +31,42 @@ echo '</pre>';
   </head>
   <body class="p-5">
     <h1>Products CRUD</h1>
-
+    <p>
+      <a href="create.php">
+        <button class="btn btn-success">Create Product</button>
+      </a>
+    </p>
     <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Image</th>
-      <th scope="col">Title</th>
-      <th scope="col">Price</th>
-      <th scope="col">Create</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>Otto</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>Thornton</td>
-      <td>Thornton</td>
-      <td>Thornton</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>Larry</td>
-      <td>Larry</td>
-      <td>Larry</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Image</th>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Create</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+
+      <?php foreach ($products as $i => $product) : ?>
+
+          <tr>
+            <th scope="row"><?php echo $i + 1 ?> </th>
+            <td></td>
+            <td><?php echo $product['title'] ?> </td>
+            <td><?php echo $product['price'] ?> </td>
+            <td><?php echo $product['create_date'] ?> </td>
+            <td>
+              <button class="btn btn-sm btn-outline-primary">Edit</button>
+              <button class="btn btn-sm btn-outline-danger">Delete</button>
+            </td>
+          </tr>
+
+      <?php endforeach; ?>
+
+      </tbody>
+    </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 
     
